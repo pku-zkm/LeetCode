071bf8690e565391b1987c9eab4e847056960b1c
Kemeng Zhu <zkm@pku.edu.cn>
2018-09-10 16:37:38 +0800

    first

--- top-interview-questions-easy/array/removeDuplicates.py
+++ top-interview-questions-easy/array/removeDuplicates.py
@@ -1,37 +1,48 @@
 class Solution:
-    def removeDuplicates(self,nums):
+ 
+    def removeDuplicates_1(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
-        
-        def move_element(arr,i,j):
-            if i<0 or j >= len(arr):
+
+        def move_element(arr, i, j):
+            if i < 0 or j >= len(arr):
                 return
-            x=arr[i]
-            for k in range(i+1,j+1):
-                arr[k-1]=arr[k]
-            arr[j]=x
+            x = arr[i]
+            for k in range(i+1, j+1):
+                arr[k-1] = arr[k]
+            arr[j] = x
 
-
-        i=1
-        j=len(nums)-1
-        if len(nums)<1:
+        i = 1
+        j = len(nums)-1
+        if len(nums) < 1:
             return 0
-        a=nums[0]
-        n=1
-        while i<=j :
-            if nums[i]==a:
-                move_element(nums,i,j)
-                j-=1
+        a = nums[0]
+        n = 1
+        while i <= j:
+            if nums[i] == a:
+                move_element(nums, i, j)
+                j -= 1
             else:
-                a=nums[i]
-                i+=1
-                n+=1
+                a = nums[i]
+                i += 1
+                n += 1
         return n
+ 
+    def removeDuplicates(self, nums):
+        now=None
+        j=0
+        for i in range(len(nums)):
+            if nums[i]!=now:
+                now=nums[i]
+                nums[j]=nums[i]
+                j+=1
+        return j
 
-s=Solution()
-arr=[1,1,2,2,2,3,3]
-n=s.removeDuplicates(arr)
-233
-print(n,arr)
+
+
+s = Solution()
+arr = [1, 1, 2, 2, 2, 3, 3]
+n = s.removeDuplicates(arr)
+print(n, arr)
